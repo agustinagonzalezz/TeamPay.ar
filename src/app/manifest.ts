d264530c2f90e247c1ab33059d94ba7a4cc96273
agus gonzalez <2313442@ucc.edu.ar>
2026-06-02 17:23:34 -0300

    .

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -11,12 +11,12 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#7c3aed",
     icons: [
       {
-        src: "/icon.png",
+        src: "/icon",
         sizes: "32x32",
         type: "image/png",
       },
       {
-        src: "/apple-icon.png",
+        src: "/apple-icon",
         sizes: "180x180",
         type: "image/png",
       },
